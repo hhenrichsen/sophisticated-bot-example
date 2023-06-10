c7519cca052ec6aa58b2fca6257d338924a4e194
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-06-09 20:21:25 -0600

    :sparkles: Allow updating the cached repo values

--- bot/src/repositories/cachedrepo.ts
+++ bot/src/repositories/cachedrepo.ts
@@ -15,6 +15,10 @@ export abstract class CachedRepo<T extends ObjectLiteral> {
         });
     }
 
+    protected async updateCache(id: string, value: T) {
+        this.cache.set(id, value);
+    }
+
     protected async findOneCached(
         id: string,
         options: FindOneOptions<T>
