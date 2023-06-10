66b9df19f431ba7a27a2714e9ac59b65e184e761
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-06-09 19:44:44 -0600

    :heavy_plus_sign: Add dependency on node-cache. Remove timed-cache

--- bot/src/repositories/cachedrepo.ts
+++ bot/src/repositories/cachedrepo.ts
@@ -1,31 +1,31 @@
 import { FindOneOptions, ObjectLiteral, Repository } from 'typeorm';
 import { Environment } from '../service/environment';
-import Cache from 'timed-cache';
+import Cache from 'node-cache';
+import NodeCache from 'node-cache';
 
 export abstract class CachedRepo<T extends ObjectLiteral> {
-    private readonly cache: Cache<T>;
+    private readonly cache: NodeCache;
 
     constructor(
         private readonly environment: Environment,
         protected readonly repo: Repository<T>
     ) {
         this.cache = new Cache({
-            defaultTtl: this.environment.get(
-                'DB_CACHE_TTL_SECONDS',
-                300,
-                parseInt
-            ),
+            stdTTL: this.environment.get('DB_CACHE_TTL_SECONDS', 300, parseInt),
         });
     }
 
-    protected async findOneCached(id: string, options: FindOneOptions<T>) {
-        const cached = this.cache.get(id);
+    protected async findOneCached(
+        id: string,
+        options: FindOneOptions<T>
+    ): Promise<T | undefined> {
+        const cached = this.cache.get(id) as T;
         if (cached) {
             return cached;
         }
         const value = (await this.repo.findOne(options)) ?? undefined;
         if (value) {
-            this.cache.put(id, value);
+            this.cache.set(id, value);
         }
         return value;
     }
