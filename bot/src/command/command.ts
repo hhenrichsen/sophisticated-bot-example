1d808d6fa51271f36a56d8690ee70344ceb1c62d
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-06-09 18:35:54 -0600

    :sparkles: Allow access to guild in command handling

--- bot/src/command/command.ts
+++ bot/src/command/command.ts
@@ -4,11 +4,13 @@ import {
     RESTPostAPIApplicationCommandsJSONBody,
 } from 'discord.js';
 import { Constructor } from '../types/constructor';
+import { Guild } from '../entities/guild.entity';
 
 export abstract class Command {
     public abstract readonly declaration: RESTPostAPIApplicationCommandsJSONBody;
     protected abstract type: Constructor<Command>;
     public abstract run(
-        interaction: ChatInputCommandInteraction<CacheType>
+        interaction: ChatInputCommandInteraction<CacheType>,
+        guild?: Guild | undefined
     ): Promise<void> | void;
 }
--- bot/src/service/command/commandhandler.ts
+++ bot/src/service/command/commandhandler.ts
@@ -2,21 +2,27 @@ import Logger from 'bunyan';
 import { ChatInputCommandInteraction, CacheType } from 'discord.js';
 import { Service } from 'typedi';
 import { CommandRegistry } from './commandregistry';
+import { GuildRepository } from '../../repositories/guild.repository';
 
 @Service()
 export class CommandHandler {
     constructor(
         private readonly logger: Logger,
-        private readonly commandRegistry: CommandRegistry
+        private readonly commandRegistry: CommandRegistry,
+        private readonly guildRepo: GuildRepository
     ) {}
 
-    public handle(interaction: ChatInputCommandInteraction<CacheType>) {
+    public async handle(interaction: ChatInputCommandInteraction<CacheType>) {
         this.logger.debug(`Handling command ${interaction.commandName}`);
+        const guild =
+            (interaction.guildId &&
+                (await this.guildRepo.getGuildById(interaction.guildId))) ||
+            undefined;
         const command = this.commandRegistry.getCommand(
             interaction.commandName
         );
         if (command) {
-            command.run(interaction);
+            command.run(interaction, guild);
         } else {
             this.logger.warn(
                 `Trying to execute nonexistent command ${interaction.commandName}`
